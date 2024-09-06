61a09118da53aa94c376f9477245d9baf68c2213
Victortimbwa <victortimbwa@gmail.com>
2024-09-06 23:26:54 +0300

    Basic annotations -concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!usr/bin/env python3
+"""
+Type annonated function
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Function that concatenate two strings
+    """
+    return str1 + str2
