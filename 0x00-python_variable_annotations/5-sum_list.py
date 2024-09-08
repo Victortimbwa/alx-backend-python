d678f95a43c45d0a21bc01299bffe46c6a01c2b2
Victortimbwa <victortimbwa@gmail.com>
2024-09-08 18:06:24 +0300

    Complex types - lisd of floats

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Annotate a function's parameters and return values
+"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    Calculate the sum of a list of floats
+    """
+    return sum(input_list)
