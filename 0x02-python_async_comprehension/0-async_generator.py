2a7a215165b8afa660f5b4c1d043381f23515562
Victortimbwa <victortimbwa@gmail.com>
2024-09-11 00:51:22 +0300

    Async Generator

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+0. Async Generator
+"""
+import random
+import asyncio
+
+
+async def async_generator():
+    """Generator that yields 10 random numbers between 0 and 10"""
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
